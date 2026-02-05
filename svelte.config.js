35b5d948df746a9e813521294c8ad6403c811b08
Mortiarion <andre-endju@ukr.net>
2026-02-05 13:21:11 +0200

    adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(
 			{
-				// fallback: 'index.html' // розкоментувати при адаптері статік для того щоб забілдити а інакше адаптер авто
+				fallback: 'index.html' // розкоментувати при адаптері статік для того щоб забілдити а інакше адаптер авто
 			}
 		)
 		// {
